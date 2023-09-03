3bb4027a09b013b6437d6404d65796274abf2037
Michelo Axel <amichelo@Air-de-Michelo.lan>
2023-09-03 20:46:00 +0200

    refactor(cors) add cors to back server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const pokedexRoute = require('./routes/pokedex');
-const cron = require('./cron')
+const cron = require('./cron');
+const cors = require('cors'); 
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: 'https://pokeguess.fun', // Remplacez par votre domaine front-end
+  methods: 'GET,HEAD',
+};
+
+app.use(cors(corsOptions)); // Utilisation du middleware CORS
+
 app.use('/api', pokedexRoute);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 cron.job();
