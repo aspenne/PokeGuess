5d372570ba1cba49f453a4abb6cee5f75ff3b6fa
Michelo Axel <amichelo@Air-de-Michelo.lan>
2023-09-07 00:26:06 +0200

    fix(cron): database name error

--- server/cron.js
+++ server/cron.js
@@ -1,6 +1,7 @@
 const schedule = require("node-schedule");
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
+const moment = require("moment-timezone");
 
 dotenv.config();
 
@@ -18,40 +19,48 @@ const dbConnection = mysql.createPool({
 
 
 function job(){
-  schedule.scheduleJob("0 0 * * *", () => {
-    const randomIdClassic = Math.floor(Math.random() * 1010) + 1;
-    const randomIdShadow = Math.floor(Math.random() * 1010) + 1;
-    const randomIdShiny = Math.floor(Math.random() * 1010) + 1;
+  schedule.scheduleJob("00 22 * * *", () => {
+    console.log('schedule run');
+    const randomIdClassic = Math.floor(Math.random() * 151) + 1;
+    const randomIdShadow = Math.floor(Math.random() * 151) + 1;
+    const randomIdShiny = Math.floor(Math.random() * 151) + 1;
 
     const insertQueryClassic = `
-      INSERT INTO PokeGuess.ClassicDaily (id, Daily)
+      INSERT INTO u697843051_PokeGuess.ClassicDaily (id, Daily)
       VALUES (?, ?);
     `;
     const insertQueryShadow = `
-      INSERT INTO PokeGuess.ShadowDaily (id, Daily)
+      INSERT INTO u697843051_PokeGuess.ShadowDaily (id, Daily)
       VALUES (?, ?);
     `;
     const insertQueryShiny = `
-      INSERT INTO PokeGuess.ShinyDaily (id, Daily)
+      INSERT INTO u697843051_PokeGuess.ShinyDaily (id, Daily)
       VALUES (?, ?);
     `;
+
+    const parisTime = moment().tz("Europe/Paris");
+    const currentDate = parisTime.format("YYYY-MM-DD"); 
 
     const insertValues = [randomIdClassic, currentDate];
 
     dbConnection.query(insertQueryClassic, insertValues, (err, results) => {
       if (err) {
+        console.log(err)
       } else {
         insertValues[0] = randomIdShadow;
 
         dbConnection.query(insertQueryShadow, insertValues, (err, results) => {
           if (err) {
+            console.log(err)
           } else {
             insertValues[0] = randomIdShiny;
 
             dbConnection.query(insertQueryShiny, insertValues, (err, results) => {
               if (err) {
+                console.log(err)
               } else {
                 const successMessage = `Inserted data into daily tables on ${currentDate}\n\tpokemon classic : ${randomIdClassic}\n\tpokemon shadow : ${randomIdShadow}\n\tpokemon hard : ${randomIdShiny}\n`;
+                console.log(successMessage);
               }
             });
           }
